dff1062938aafff9b3199ef8a0fe851df1e1926d
Avijit Bag <90190061+AvijitBag@users.noreply.github.com>
2023-08-30 15:34:10 +0530

    Delete compile.sh

--- guided_concurrentKernels_SYCLMigration/02_sycl_migrated/Samples/0_Introduction/concurrentKernels/compile.sh
+++
@@ -1,4 +0,0 @@
-#icpx -fsycl -fsycl-targets=nvptx64-nvidia-cuda *.cpp -I ../../../Common/ -I ../../../include/
-icpx -fsycl -fsycl-targets=nvptx64-nvidia-cuda *.cpp -I ../../../Common/ 
-#icpx  -fsycl -fsycl-targets=intel_gpu_pvc -Xsycl-target-backend '-options -ze-intel-enable-auto-large-GRF-mode' *.cpp -I ../../../Common/ 
-#icpx  -fsycl  *.cpp -I ../../../Common/ 
